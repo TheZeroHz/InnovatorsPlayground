9ebb2762ffb700752c155cf6058c105e754c10c1
Rakib Hasan <59503211+TheZeroHz@users.noreply.github.com>
2025-02-27 20:27:04 +0600

    Create UkraneFlag.py

---
+++ Python/Class4/UkraneFlag.py
@@ -0,0 +1,29 @@
+from turtle import *
+## Draw BD Flag Using Loop // Ctrl+C=Copy   Ctrl+V=Paste 
+##range(start->d0,end, inc/dec->d1)
+fillcolor('yellow')
+begin_fill()
+for i in range(4):
+    if i==0 or i==2:
+        forward(350)
+        left(90)
+    else:
+        forward(100)
+        left(90)
+end_fill()
+
+penup()
+goto(0,100)
+pendown()
+
+fillcolor('yellow')
+begin_fill()
+for i in range(4):
+    if i==0 or i==2:
+        forward(350)
+        left(90)
+    else:
+        forward(100)
+        left(90)
+end_fill()
+
